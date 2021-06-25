aef58a28141cf742412aaa1497c9fc236c44fa0c
netuno <rianadao@outlook.com>
2021-06-25 15:37:34 -0300

    obtendo horarios

---
+++ topicos especiais/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace topicos_especiais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+           
+            DateTime hora = DateTime.Now;
+            Console.WriteLine(hora);
+            TimeSpan t1 = new TimeSpan(100000000);
+            DateTime t2 = new DateTime(2021, 8, 15, 13, 45, 58, 257);
+            Console.WriteLine("day of year:"+t2.DayOfYear);
+            Console.WriteLine("day of week:" + t2.DayOfWeek);
+
+            
+            Console.WriteLine(t1);
+        }
+    }
+}
