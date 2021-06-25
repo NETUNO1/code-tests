c9656580dd4e4612e92d3bafe9e81e7ff78a75e8
netuno <rianadao@outlook.com>
2021-06-25 15:55:14 -0300

    faz uma soma de dois numeros

--- topicos especiais/Program.cs
+++ topicos especiais/Program.cs
@@ -12,9 +12,18 @@ namespace topicos_especiais
             DateTime t2 = new DateTime(2021, 8, 15, 13, 45, 58, 257);
             Console.WriteLine("day of year:"+t2.DayOfYear);
             Console.WriteLine("day of week:" + t2.DayOfWeek);
-
-            
             Console.WriteLine(t1);
+            int soma = 0;
+            for (int i = 1; i < 3; i++)
+            {
+                
+                Console.WriteLine("entre com tres numeros");
+                Console.WriteLine("num" + i + ":");
+                int n = int.Parse(Console.ReadLine());
+                soma += n;
+                
+            }
+            Console.WriteLine(soma);
         }
     }
 }
